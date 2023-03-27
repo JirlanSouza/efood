f68e1ce6c938cb8bc3eaf9dbd926cdd8365b43de
Jirlan Souza <jirlansouza08@gmail.com>
2023-03-27 16:37:48 -0400

    initial commit

---
+++ src/main/java/com/efood/EfoodApplication.java
@@ -0,0 +1,13 @@
+package com.efood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfoodApplication.class, args);
+    }
+
+}
